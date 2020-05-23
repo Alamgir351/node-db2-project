f38ddac01b347cc5a4c6ebfd39683b0d751d0f79
thomas <t_huynh927@yahoo.com>
2020-05-22 22:41:51 -0700

    did CRUD features

--- cars/carsDB.js
+++ cars/carsDB.js
@@ -1,9 +1,32 @@
 const db = require('../data/dbconfig')
 
 module.exports = {
-    get
+    get,
+    getById,
+    insertCar,
+    update,
+    remove
 }
 
 function get() {
     return db('cars')
+}
+
+function getById(id) {
+    return db('cars').where({id})
+}
+
+async function insertCar(car) {
+    const res = await db('cars').insert(car)
+    if (res.rowCount >= 1) { return car }
+}
+
+function update(id, car) {
+    return db('cars').where({id}).update(car)
+}
+
+async function remove(id) {
+    const car = await getById(id)
+    db('cars').where({id}).del().then(res => console.log(res))
+    return car
 }
--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -13,4 +13,51 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const car = await db.getById(id)
+        if (car.length > 0) { return res.status(200).json(car) }
+        return res.status(400).json({ message: "O_o Something ain't right!" })
+    } catch(e) {
+        res.status(500).json({ message: ";-; We got a problem here. "})
+    }
+})
+
+router.post('/', async (req, res) => {
+    const body = req.body
+    try {
+        const car = await db.insertCar(body)
+        if (car) { return res.status(200).json(car) }
+        return res.status(400).json({ message: "O_o Something ain't right!" })
+    } catch(e) {
+        res.status(500).json({ message: ";-; We got a problem here. "})
+    }
+})
+
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const car = await db.getById(id)
+        if (car) { return res.status(200).json(car) }
+        return res.status(400).json({ message: "O_o Something ain't right!" })
+    } catch(e) {
+        res.status(500).json({ message: ";-; We got a problem here. "})
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id
+    const body = req.body
+    try {
+        const car = await db.update(id, body)
+        if (car >= 1) { return res.status(200).json(body) }
+        return res.status(400).json({ message: "O_o Something ain't right!" })
+    } catch(e) {
+        res.status(500).json({ message: ";-; We got a problem here. "})
+    }
+})
+
+
 module.exports = router
